7b1452e5c2e5810d3ea6c9c9037bc9d6fbdeeed8
alee <alee046@ucr.edu>
2015-12-17 14:45:25 -0800

    styling

--- chat.js
+++ chat.js
@@ -1,44 +1,8 @@
+$( document ).ready(function() {
 
-    var uName;
     var messages = document.getElementById('messages');
     var newMsg = document.getElementById('new-msg');
-//   var userName = uName;
-
-    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-
-    var icons = {
-
-           deals: iconBase + 'dollar.png',
-           info: iconBase + 'info.png',
-           help: iconBase + 'mechanic.png',
-           poi: iconBase + 'info_circle.png',
-           caution: iconBase + 'caution.png'
-
-        };
-
-    while (!uName) {
-      uName = prompt("Please enter your initials")
-      console.log(uName);
-    };
-
-    function guid() {
-
-      function s4() { return Math.floor((1 + Math.random()) * 0x10000)
-          .toString(16).substring(1);
-
-      };
-
-      return s4() + s4() + '-' + s4() + '-' + s4() + s4();
-    }
-
-var userInfo = {
-
-    id: guid(),
-    name: uName + (navigator.platform? ' ('+navigator.platform+')':'')
-    icon: uIcon
-    }
-
-  var socket = io('https://shrouded-brook-8349.herokuapp.com/');
+    var socket = io('https://shrouded-brook-8349.herokuapp.com/');
 
   socket.on('add-message', function (data) {
     addMessage(data);
@@ -57,9 +21,9 @@ function addChatTyping (data) {
   }
 
     // Removes the visual chat typing message
-  function removeChatTyping (uName) {
-        self.messages = self.messages.filter(function(element){return element.uName != uName || element.content != " is typing"})
-  }
+function removeChatTyping (uName) {
+      self.messages = self.messages.filter(function(element){return element.uName != uName || element.content != " is typing"})
+}
 
   document.getElementById('btn-send-msg').addEventListener('click', function() {
     socket.emit('add-message', {
@@ -73,3 +37,4 @@ function addChatTyping (data) {
     messages.innerHTML += ['<li><strong>', data.name, ':</strong> ', data.msg + '</li>'].join('');
   }
 
+});
